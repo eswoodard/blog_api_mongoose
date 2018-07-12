3efa38823d1a2ab9cc8b86998c61d33b9b606698
Elizabeth Woodard <eswoodard75@gmail.com>
2018-07-12 17:56:09 -0400

    completed server.js

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require("/.config");
+const blogPostRouter = require('./blogPostRouter');
+
+app.use(morgan('common'));
+app.use('/blog-post', blogPostRouter);
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(
+            databaseUrl,
+            err => {
+                if (err) {
+                    return reject(err);
+                }
+                server = app.listen(port, () => {
+                    console.log(`Your app is listening on port ${port}`);
+                    resolve();
+                })
+                .on("error", err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+            }
+        );
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log("Closing Server");
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
+
